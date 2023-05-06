e56480dc67c35f52bbf3764904bfcd3521475276
vVolodya <vladimir.yevseev@gmail.com>
2023-05-06 22:04:42 +1000

    [+] Prettier

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from '@remix-run/node';
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: 'New Remix App' }];
 };
 
 export default function Index() {
