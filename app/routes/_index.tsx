ed4f292f5919cb4ff6bfac7104f30f36c6eef8bb
vVolodya <vladimir.yevseev@gmail.com>
2023-05-06 19:19:23 +1000

    [+] INIT COMMIT BABY

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return <div className="bg-gray-200 text-red-500">Hello world!</div>;
+}
